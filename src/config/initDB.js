bb7ae040bf15798a6fc0539bff5b9cf23ec23e4b
Leonardo Romeiro <leozinwork@gmail.com>
2024-08-03 14:33:19 -0300

    Corrigindo auto incremente e criando tabela usuario

--- src/config/initDB.js
+++ src/config/initDB.js
@@ -5,58 +5,67 @@ const createTables = async () => {
     const pool = await poolPromise;
     
     const createOrdensTable = `
-      -- Verifica se as tabelas não existe e cria
-      IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Clientes' and xtype='U')
-      BEGIN
-          CREATE TABLE Clientes (
-              ClienteID INT PRIMARY KEY AUTO_INCREMENT,
-              Nome VARCHAR(255) NOT NULL,
-              CNPJ CHAR(14) NOT NULL UNIQUE
-          );
-      END
+    -- Verifica se a tabela Clientes não existe e cria a tabela
+    IF OBJECT_ID('Clientes', 'U') IS NULL
+    BEGIN
+        CREATE TABLE Clientes (
+            ClienteID INT PRIMARY KEY IDENTITY(1,1),
+            Nome VARCHAR(255) NOT NULL,
+            CNPJ CHAR(14) NOT NULL UNIQUE
+        );
+    END
 
-      IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Funcionários' and xtype='U')
-      BEGIN
-          CREATE TABLE Funcionários (
-              FuncionarioID INT PRIMARY KEY AUTO_INCREMENT,
-              Nome VARCHAR(255) NOT NULL,
-              CPF CHAR(11) NOT NULL UNIQUE
-          );
-      END
+    IF OBJECT_ID('Funcionários', 'U') IS NULL
+    BEGIN
+        CREATE TABLE Funcionários (
+            FuncionarioID INT PRIMARY KEY IDENTITY(1,1),
+            Nome VARCHAR(255) NOT NULL,
+            CPF CHAR(11) NOT NULL UNIQUE
+        );
+    END
 
-      IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Projetos' and xtype='U')
-      BEGIN
-          CREATE TABLE Projetos (
-              ProjetoID INT PRIMARY KEY AUTO_INCREMENT,
-              Nome VARCHAR(255) NOT NULL,
-              ClienteID INT,
-              FOREIGN KEY (ClienteID) REFERENCES Clientes(ClienteID)
-          );
-      END
+    IF OBJECT_ID('Projetos', 'U') IS NULL
+    BEGIN
+        CREATE TABLE Projetos (
+            ProjetoID INT PRIMARY KEY IDENTITY(1,1),
+            Nome VARCHAR(255) NOT NULL,
+            ClienteID INT,
+            FOREIGN KEY (ClienteID) REFERENCES Clientes(ClienteID)
+        );
+    END
 
-      IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Atividades' and xtype='U')
-      BEGIN
-          CREATE TABLE Atividades (
-              AtividadeID INT PRIMARY KEY AUTO_INCREMENT,
-              Nome VARCHAR(255) NOT NULL,
-              Descricao TEXT
-          );
-      END
+    IF OBJECT_ID('Atividades', 'U') IS NULL
+    BEGIN
+        CREATE TABLE Atividades (
+            AtividadeID INT PRIMARY KEY IDENTITY(1,1),
+            Nome VARCHAR(255) NOT NULL,
+            Descricao TEXT
+        );
+    END
 
-      IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='OrdemDeServico' and xtype='U')
-      BEGIN
-          CREATE TABLE OrdemDeServico (
-              OrdemID INT PRIMARY KEY AUTO_INCREMENT,
-              ProjetoID INT,
-              FuncionarioID INT,
-              Data DATE NOT NULL,
-              AtividadeID INT,
-              Complemento TEXT,
-              FOREIGN KEY (ProjetoID) REFERENCES Projetos(ProjetoID),
-              FOREIGN KEY (FuncionarioID) REFERENCES Funcionários(FuncionarioID),
-              FOREIGN KEY (AtividadeID) REFERENCES Atividades(AtividadeID)
-          );
-      END
+    IF OBJECT_ID('OrdemDeServico', 'U') IS NULL
+    BEGIN
+        CREATE TABLE OrdemDeServico (
+            OrdemID INT PRIMARY KEY IDENTITY(1,1),
+            ProjetoID INT,
+            FuncionarioID INT,
+            Data DATE NOT NULL,
+            AtividadeID INT,
+            Complemento TEXT,
+            FOREIGN KEY (ProjetoID) REFERENCES Projetos(ProjetoID),
+            FOREIGN KEY (FuncionarioID) REFERENCES Funcionários(FuncionarioID),
+            FOREIGN KEY (AtividadeID) REFERENCES Atividades(AtividadeID)
+        );
+    END
+
+    IF OBJECT_ID('Usuario', 'U') IS NULL
+    BEGIN
+        CREATE TABLE Usuario (
+            UsuarioID INT PRIMARY KEY IDENTITY(1,1),
+            Senha VARCHAR(50),
+            Email VARCHAR(50),
+        );
+    END
 
     `;
 
