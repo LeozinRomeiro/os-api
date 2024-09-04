64b9c90766eac5f4a5f5d19319afa0ea595a2dfc
Rhuan Pablo <rhuan.p.f@hotmail.com>
2024-09-04 19:07:04 -0300

    Ajuste nos CRUD e ajustado o Sequelize nas classes

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,31 +1,41 @@
-const Usuario = require('../models/usuario');
-const { compare } = require('bcryptjs');
-const { sign } = require('jsonwebtoken');
-const config = require('config');
+const { Usuario, UsuariosSequelize } = require('../models/usuario')
+const { compare } = require('bcryptjs')
+const { sign } = require('jsonwebtoken')
+const config = require('config')
 
-class AuthService{
-    async login(dto){
-        const usuario = await Usuario.getByEmail(dto.email);
+class AuthService {
+  async login(dto) {
+    console.log(dto)
+    const usuario = await UsuariosSequelize.findOne({
+      where: {
+        Email: dto.email
+      }
+    })
 
-        if (!usuario) {
-            throw new Error('Usuario não encontrado');
-        }
-
-        const authenticated = await compare(dto.senha, usuario.Senha);
-
-        if (!authenticated) {
-            throw new Error('Usuario ou senha incorreto');
-        }
-        
-        const accessToken = sign({
-            id: usuario.id,
-            name: usuario.name
-        },config.get("secret"),{
-            expiresIn: 86400
-        })
-
-        return { accessToken };
+    if (!usuario) {
+      throw new Error('Usuario não encontrado')
     }
+
+    const authenticated = await compare(dto.senha, usuario.Senha)
+    console.log(authenticated)
+
+    if (authenticated) {
+      throw new Error('Usuario ou senha incorreto')
+    }
+
+    const accessToken = sign(
+      {
+        id: usuario.id,
+        name: usuario.name
+      },
+      config.get('secret'),
+      {
+        expiresIn: 86400
+      }
+    )
+
+    return { accessToken }
+  }
 }
 
-module.exports = AuthService;
+module.exports = AuthService
